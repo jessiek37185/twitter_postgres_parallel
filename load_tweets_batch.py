40e41df7b38b7c04215df157970287e33b78d549
Ka Hei (Jessie) Kong <KKong76@students.claremontmckenna.edu>
2026-04-14 22:11:59 -0700

    Update load_tweets_batch.py

--- load_tweets_batch.py
+++ load_tweets_batch.py
@@ -13,150 +13,81 @@ def remove_nulls(s):
     return s.replace('\x00','\\x00')
 
 
-def batch(iterable, n=1):
-    l = len(iterable)
-    for ndx in range(0, l, n):
-        yield iterable[ndx:min(ndx + n, l)]
-
-
-def insert_tweets(connection, tweets, batch_size=1000):
-    for i, tweet_batch in enumerate(batch(tweets, batch_size)):
-        print(datetime.datetime.now(), 'batch', i)
-        _insert_tweets(connection, tweet_batch)
-
-
-def _insert_tweets(connection, input_tweets):
+def insert_tweets(connection, tweets):
 
     users = []
-    tweets = []
+    users_unhydrated = []
     tweet_mentions = []
-    tweet_tags = []
-    tweet_media = []
+    tweets_data = []
 
-    ########################################
-    # STEP 1: build lists
-    ########################################
-    for tweet in input_tweets:
+    for tweet in tweets:
 
+        # full user
         users.append({
             'id_users': tweet['user']['id'],
-            'created_at': tweet['user']['created_at'],
-            'updated_at': tweet['created_at'],
             'screen_name': remove_nulls(tweet['user']['screen_name']),
-            'name': remove_nulls(tweet['user']['name']),
-            'location': remove_nulls(tweet['user']['location']),
-            'description': remove_nulls(tweet['user']['description']),
-            'protected': tweet['user']['protected'],
-            'verified': tweet['user']['verified'],
-            'friends_count': tweet['user']['friends_count'],
-            'listed_count': tweet['user']['listed_count'],
-            'favourites_count': tweet['user']['favourites_count'],
-            'statuses_count': tweet['user']['statuses_count'],
-            'withheld_in_countries': tweet['user'].get('withheld_in_countries'),
         })
 
-        # GEO SAFE
-        geo_str = None
-        geo_coords = None
+        # mentions (fix FK issue)
+        for m in tweet['entities']['user_mentions']:
+            users_unhydrated.append({
+                'id_users': m['id'],
+                'screen_name': remove_nulls(m.get('screen_name'))
+            })
+
+            tweet_mentions.append({
+                'id_tweets': tweet['id'],
+                'id_users': m['id']
+            })
+
+        # tweet
+        geo = None
         try:
             coords = tweet['geo']['coordinates']
-            geo_coords = f"{coords[0]} {coords[1]}"
-            geo_str = 'POINT'
+            geo = f"POINT({coords[0]} {coords[1]})"
         except:
             pass
 
-        try:
-            text = tweet['extended_tweet']['full_text']
-        except:
-            text = tweet['text']
+        text = tweet.get('text')
+        if 'extended_tweet' in tweet:
+            text = tweet['extended_tweet'].get('full_text', text)
 
-        tweets.append({
+        tweets_data.append({
             'id_tweets': tweet['id'],
             'id_users': tweet['user']['id'],
             'created_at': tweet['created_at'],
-            'geo_str': geo_str,
-            'geo_coords': geo_coords,
             'text': remove_nulls(text),
-            'lang': tweet.get('lang'),
-            'source': remove_nulls(tweet.get('source')),
+            'geo': geo
         })
 
-        # mentions
-        for mention in tweet['entities']['user_mentions']:
-            tweet_mentions.append({
-                'id_tweets': tweet['id'],
-                'id_users': mention['id']
-            })
-
-        # tags
-        for tag in tweet['entities']['hashtags']:
-            tweet_tags.append({
-                'id_tweets': tweet['id'],
-                'tag': '#' + tag['text']
-            })
-
-    ########################################
-    # STEP 2: insert (ONE transaction only)
-    ########################################
     with connection.begin():
 
-        if users:
-            connection.execute(sqlalchemy.text('''
-                INSERT INTO users (id_users, created_at, updated_at,
-                    screen_name, name, location, description,
-                    protected, verified, friends_count,
-                    listed_count, favourites_count, statuses_count,
-                    withheld_in_countries)
-                VALUES (:id_users, :created_at, :updated_at,
-                    :screen_name, :name, :location, :description,
-                    :protected, :verified, :friends_count,
-                    :listed_count, :favourites_count, :statuses_count,
-                    :withheld_in_countries)
-                ON CONFLICT (id_users) DO NOTHING
-            '''), users)
+        connection.execute(sqlalchemy.text('''
+            INSERT INTO users (id_users, screen_name)
+            VALUES (:id_users, :screen_name)
+            ON CONFLICT (id_users) DO NOTHING
+        '''), users)
 
-        if tweets:
-            sql = '''
-                INSERT INTO tweets
-                (id_tweets,id_users,created_at,geo,text,lang,source)
-                VALUES
-            '''
-            values = []
-            binds = {}
+        connection.execute(sqlalchemy.text('''
+            INSERT INTO users (id_users, screen_name)
+            VALUES (:id_users, :screen_name)
+            ON CONFLICT (id_users) DO NOTHING
+        '''), users_unhydrated)
 
-            for i, t in enumerate(tweets):
-                values.append(f"""(
-                    :id_tweets{i},
-                    :id_users{i},
-                    :created_at{i},
-                    CASE WHEN :geo_str{i} IS NULL THEN NULL
-                        ELSE ST_GeomFromText(:geo_str{i} || '(' || :geo_coords{i} || ')',4326)
-                    END,
-                    :text{i},
-                    :lang{i},
-                    :source{i}
-                )""")
+        connection.execute(sqlalchemy.text('''
+            INSERT INTO tweets (id_tweets, id_users, created_at, text, geo)
+            VALUES (
+                :id_tweets, :id_users, :created_at, :text,
+                CASE WHEN :geo IS NULL THEN NULL ELSE ST_GeomFromText(:geo,4326) END
+            )
+            ON CONFLICT (id_tweets) DO NOTHING
+        '''), tweets_data)
 
-                for k, v in t.items():
-                    binds[f"{k}{i}"] = v
-
-            sql += ",".join(values) + " ON CONFLICT (id_tweets) DO NOTHING"
-
-            connection.execute(sqlalchemy.text(sql), binds)
-
-        if tweet_mentions:
-            connection.execute(sqlalchemy.text('''
-                INSERT INTO tweet_mentions (id_tweets,id_users)
-                VALUES (:id_tweets,:id_users)
-                ON CONFLICT DO NOTHING
-            '''), tweet_mentions)
-
-        if tweet_tags:
-            connection.execute(sqlalchemy.text('''
-                INSERT INTO tweet_tags (id_tweets,tag)
-                VALUES (:id_tweets,:tag)
-                ON CONFLICT DO NOTHING
-            '''), tweet_tags)
+        connection.execute(sqlalchemy.text('''
+            INSERT INTO tweet_mentions (id_tweets, id_users)
+            VALUES (:id_tweets, :id_users)
+            ON CONFLICT DO NOTHING
+        '''), tweet_mentions)
 
 
 if __name__ == '__main__':
@@ -165,16 +96,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--db', required=True)
     parser.add_argument('--inputs', nargs='+', required=True)
-    parser.add_argument('--batch_size', type=int, default=1000)
     args = parser.parse_args()
 
     engine = sqlalchemy.create_engine(args.db)
     connection = engine.connect()
 
-    for filename in sorted(args.inputs, reverse=True):
+    for filename in args.inputs:
         with zipfile.ZipFile(filename, 'r') as archive:
-            print(datetime.datetime.now(), filename)
-            for subfilename in sorted(archive.namelist(), reverse=True):
+            for subfilename in archive.namelist():
                 with io.TextIOWrapper(archive.open(subfilename)) as f:
                     tweets = [json.loads(line) for line in f]
-                    insert_tweets(connection, tweets, args.batch_size)
+                    insert_tweets(connection, tweets)
