2e63dcdc6b5027675b74a28c506d2cef2d88043c
Ka Hei (Jessie) Kong <KKong76@students.claremontmckenna.edu>
2026-04-15 01:04:59 -0700

    Update load_tweets_parallel.sh

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=$(find data/*)
+files=$(find data)
 
 echo '================================================================================'
 echo 'load pg_denormalized (parallel)'
@@ -10,13 +10,13 @@ time echo "$files" | parallel -j 10 ./load_denormalized.sh {}
 echo '================================================================================'
 echo 'load pg_normalized (parallel)'
 echo '================================================================================'
-time echo "$files" | parallel -j 10 python3 load_tweets.py \
+time find data -type f | parallel -j 10 python3 load_tweets.py \
     --db=postgresql://postgres:pass@localhost:5439/postgres \
     --inputs {}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch (parallel)'
 echo '================================================================================'
-time echo "$files" | parallel -j 10 python3 load_tweets_batch.py \
+time find data -type f | parallel -j 10 python3 load_tweets_batch.py \
     --db=postgresql://postgres:pass@localhost:5440/postgres \
     --inputs {}
